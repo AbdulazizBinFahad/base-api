12c5c53fc1166446c0544d8ae40add15a9c68f86
unknown <94074156+AbdulazizBinFahad@users.noreply.github.com>
2025-04-14 10:42:19 +0300

    Add Users Routes to app

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from './docs/swagger';
 import authRoutes from './routes/authRoutes';
-import './types/auth';
+import usersRoutes from './routes/userRoutes';
 
 const app = express();
 
@@ -14,6 +14,9 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Authentication routes
 app.use('/api/auth', authRoutes);
+
+// Users routes
+app.use('/api/users', usersRoutes);
 
 // Root route
 app.get('/', (req: Request, res: Response) => {
